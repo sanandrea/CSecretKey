243acae49f6945c16c8cbabff1de283e719fd6cd
sanandrea <sanandrea8080@gmail.com>
2014-09-15 15:56:41 +0200

    improved test
    Python Test takes the output from stdout and compares the two outputs

--- reverse_test.py
+++ reverse_test.py
@@ -2,11 +2,27 @@ import hmac
 import binascii
 from hashlib import sha256,sha1
 import string
+import subprocess
 
+proc = subprocess.Popen(["./test",""],stdout=subprocess.PIPE)
+counter = 0
+while True:
+    line = proc.stdout.readline()
+    if line != '':
+        counter += 1
+        if counter == 1:
+            password = line[18:].rstrip()
+        if counter == 2:
+            output = line[15:-3].rstrip()
+    else:
+        break
 
-
-key = "lorux0369ADwW:zzzzz:'^-->.;*+CHMRWbglqv05+DINSXchm" 
-text = "Hi There" 
-hashed = hmac.new(key, text, sha256)
+assert counter > 2, "Are you using production build?"
+ 
+text = "Hi There"
+hashed = hmac.new(password, text, sha256)
 # The signature
-print binascii.b2a_base64(hashed.digest())[:-1]
+signature = binascii.b2a_base64(hashed.digest())[:-1]
+
+assert output == signature
+print "Test Passed!"
